fefb433500381007bd5526a5a1c0d8f6b0af056a
solrz <14904805+solrz@users.noreply.github.com>
2020-12-13 18:31:23 +0800

    Add files via upload

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "216b473c4cd895ec907c46e862e99467",
-"index.html": "6f2d74e8cef7e465870042a717e8dbdf",
-"/": "6f2d74e8cef7e465870042a717e8dbdf",
+"index.html": "9f88c5f9a46bc4d071fdc47055a1daf9",
+"/": "9f88c5f9a46bc4d071fdc47055a1daf9",
 "main.dart.js": "c6d7fcf87e485af647e5f7510bf81646",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
